bc0cfe6e52cdeff0f1d0c69769af4bf7bb8f801b
Esteban Castrillon <260802745+silencedracer360-collab@users.noreply.github.com>
2026-08-28 16:48:01 -0500

    Aprendices

--- app.js
+++ app.js
@@ -1,11 +1,72 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config() //Forma de exportar antigua CommonJS
+const port = process.env.PUERTO || 3000;
 
-app.get("/", (_, res) => {
-res.send(`Aprendicez ficha 3407186`);
+//middleware para parsear datos del body
+app.use(express.json()) //--> en formato de JSON
+app.use(express.urlencoded({extended: true})) //--> en formato de Formulario
+
+app.get("/", (req, res) => {
+    res.send(`API REST APRENDICES`);
 });
 
+//Lista aprendices
+app.get("/api/aprendices", (req, res) => {
+    //res.status(200).json es para verificar que el mensjae esta OK
+    res.status(200).json({
+        "mensaje": "Lista de Aprendices"
+    });
+});
+
+//Listar un aprendiz
+app.get("/api/aprendices/:id", (req, res) => {
+    //res.status(200).json es para verificar que el mensjae esta OK
+    res.status(200).json({
+        "mensaje": "Aprendiz 1"
+    });
+});
+
+//Endpoint crear aprendices
+app.post("/api/aprendices", (req, res) => {
+    res.status(201).json({
+        "mensaje": "Crear aprendiz"
+    });
+});
+
+//Endpoint para editar aprendices
+app.put("/api/aprendices/:id", (req, res) => {
+    res.status(200).json({
+        "mensaje": "Editar aprendiz"
+    });
+});
+//Endpoints para eliminar aprendices
+app.delete("/api/aprendices/:id", (req, res) => {
+    res.status(200).json({
+        "mensaje": "Eliminar aprendiz"
+    });
+});
+
+
+app.post("/rutaJSON", (req, res) => {
+    const todosDatos = req.body
+    const edad = req.body.edad2
+    if(edad >= 18){
+        res.json({datosJson: "Es mayor de edad"});
+    }else{
+        res.json({mensaje: ""});
+    }
+
+});
+
+app.post("/rutaFormulario", (req, res) => {
+    const todosDatos = req.body
+    const programa = req.body.programa
+    res.json({TodosDatos: todosDatos, miPrograma: programa});
+});
+
+
+
 app.listen(port, () => {
-console.log( `Servidor en funcionamiento en el puerto: ${port}`);
+    console.log( `Servidor en funcionamiento en el puerto: http://localhost:${port}`);
 });
